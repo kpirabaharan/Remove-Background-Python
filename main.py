94be851c757946c6df010ddbe5f9ae0896760a79
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-10-30 23:41:39 -0400

    Print Image Shape and Map
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+import sys
+
 import cv2
-from PIL import Image
+import numpy as np
 
 
 def main():
+    # Display Full Untruncated Image
+    np.set_printoptions(threshold=sys.maxsize)
+
     # Save image in set directory
     # Read RGB image
     img = cv2.imread("Shirts_Board.jpeg")
 
     # Output img with window name as 'image'
     cv2.imshow("image", img)
+
+    # Print Image Shape and Map
+    print(img.shape)
+    print(img)
 
     # Maintain output window until
     # user presses a key
